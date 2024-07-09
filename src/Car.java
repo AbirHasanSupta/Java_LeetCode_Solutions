2e27d3dee6e8ac881df6ee96e1013551ff55ebc2
Abir <abirhasan.supta254@gmail.com>
2024-07-09 19:30:27 +0600

    2nd

--- src/Car.java
+++ src/Car.java
@@ -10,20 +10,22 @@ public class Car implements Serializable {
     String name;
     int model;
     transient int number;
+    int seats;
     private static final long serialVersionUID = 1L;
 
-    public Car(String name, int model, int number){
+    public Car(String name, int model, int number, int seats){
         this.name = name;
         this.model = model;
         this.number = number;
+        this.seats = seats;
     }
     public String toString(){
-        return String.format("Car is %s, model is %d, number is %d", this.name, this.model, this.number);
+        return String.format("Car is %s, model is %d, number is %d and the seats are %d", this.name, this.model, this.number, this.seats);
     }
     public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         System.out.println("hello");
-        Car car1 = new Car("Mazda", 12, 1);
-        Car car2 = new Car("Supra", 98, 2);
+        Car car1 = new Car("Mazda", 12, 1, 4);
+        Car car2 = new Car("Supra", 98, 2, 5);
         FileOutputStream fileOutputStream = new FileOutputStream("public.txt");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(car1);
