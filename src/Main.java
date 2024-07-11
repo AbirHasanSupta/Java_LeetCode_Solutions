591b769a6ab82a3661d97e000b86662776ef05bb
Abir <abirhasan.supta254@gmail.com>
2024-07-12 01:31:51 +0600

    day 4

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Main{
  public static void main(String[] args){
   Solution sol = new Solution();
-  int[][] arr = {{5,2},{5,4},{10,3},{20,1}};
-  System.out.println(sol.averageWaitingTime(arr));
+//  int[][] arr = {{5,2},{5,4},{10,3},{20,1}};
+//  System.out.println(sol.averageWaitingTime(arr));
+  System.out.println(sol.reverseParentheses("(ed(et(oc))el)"));
  }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,56 @@
-public class Solution {
-    public double averageWaitingTime(int[][] customers) {
-        double res = 0;
-        int curr = 0;
-        for(int i = 0; i < customers.length; i++){
-            if (curr < customers[i][0]){
-                curr = customers[i][0] + customers[i][1];
-                res += (curr - customers[i][0]);
-                System.out.println("if"+res + " "+curr);
-            }else{
-                curr += customers[i][1];
-                res += curr - customers[i][0];
-                System.out.println("else"+res +" " + curr);
+//public class Solution {
+//    public double averageWaitingTime(int[][] customers) {
+//        double res = 0;
+//        int curr = 0;
+//        for(int i = 0; i < customers.length; i++){
+//            if (curr < customers[i][0]){
+//                curr = customers[i][0] + customers[i][1];
+//                res += (curr - customers[i][0]);
+//                System.out.println("if"+res + " "+curr);
+//            }else{
+//                curr += customers[i][1];
+//                res += curr - customers[i][0];
+//                System.out.println("else"+res +" " + curr);
+//            }
+//        }
+//        return res / customers.length;
+//
+//    }
+//}
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Stack;
+
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> pair = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i ++){
+            if(s.charAt(i) == '('){
+                stack.push(i);
+            } else if(s.charAt(i) == ')') {
+                int x = stack.pop();
+                pair.put(x, i);
+                pair.put(i, x);
             }
         }
-        return res / customers.length;
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        int dir = 1;
+        while(i < s.length()){
+            if(s.charAt(i) == ')' || s.charAt(i) == '('){
+                i = pair.get(i);
+                dir = dir * -1;
+
+            }else{
+                res.append(s.charAt(i));
+            }
+            i += dir;
+        }
+        return res.toString();
 
     }
 }
-
