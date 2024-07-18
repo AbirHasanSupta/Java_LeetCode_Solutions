e02fd2dd664e63e0b417cf7d784bf281ebd32520
Abir <abirhasan.supta254@gmail.com>
2024-07-18 13:23:26 +0600

    Threading

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 
 public class Main{
  public static void main(String[] args){
-  Solution sol = new Solution();
-//  int[][] arr = {{5,2},{5,4},{10,3},{20,1}};
-//  System.out.println(sol.averageWaitingTime(arr));
-//  System.out.println(sol.reverseParentheses("(ed(et(oc))el)"));
-  System.out.println(sol.maximumGain( "aabbaaxybbaabb", 5, 4));
+//  Solution sol = new Solution();
+////  int[][] arr = {{5,2},{5,4},{10,3},{20,1}};
+////  System.out.println(sol.averageWaitingTime(arr));
+////  System.out.println(sol.reverseParentheses("(ed(et(oc))el)"));
+//  System.out.println(sol.maximumGain( "aabbaaxybbaabb", 5, 4));
+  for(int i = 0; i < 3; i ++){
+   MultiThreading myThing = new MultiThreading(i);
+   myThing.start();
+  }
+//  MultiThreading myThing = new MultiThreading();
+//  MultiThreading myThing2 = new MultiThreading();
+//  myThing.start();
+//  myThing2.start();
  }
 }
---
+++ src/MultiThreading.java
@@ -0,0 +1,19 @@
+public class MultiThreading extends Thread{
+    private int threadNumber;
+    public MultiThreading(int threadNumber){
+        this.threadNumber = threadNumber;
+    }
+
+
+    @Override
+    public void run(){
+        for(int i = 1; i <= 5; i ++){
+            System.out.println(i+" from thread " + this.threadNumber);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
