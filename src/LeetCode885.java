3740e5a8b1c88df8cc529d35e07da5fd4e7c7c2c
Abir <abirhasan.supta254@gmail.com>
2024-08-08 21:14:08 +0600

    8 august, 24

---
+++ src/LeetCode885.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode885 {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] dir = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        int[][] res = new int[rows * cols][2];
+        int idx = 0;
+
+        for (int step = 1, direction = 0; idx < rows * cols;) {
+            for (int i = 0; i < 2; i ++) {
+                for (int j = 0; j < step; j++) {
+
+                    if (
+                            rStart >= 0 &&
+                                    rStart < rows &&
+                                    cStart >= 0 &&
+                                    cStart < cols
+                    ) {
+                        res[idx][0] = rStart;
+                        res[idx][1] = cStart;
+                        idx ++;
+                    }
+
+                    rStart += dir[direction][0];
+                    cStart += dir[direction][1];
+                }
+
+                direction = (direction + 1) % 4;
+            }
+            step ++;
+        }
+        return res;
+    }
+}
